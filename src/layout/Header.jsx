81df4a84d8cbc2ee0f52b8e5866a24d99c9e9486
Artur <tsebaevartur38@gmail.com>
2021-09-30 09:07:13 +0300

    adaptive added

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,5 @@
 function Header(){
-    return  <nav className='cyan darken-3'>
+    return  <nav className='cyan container darken-3'>
     <div className="nav-wrapper">
       <span className="brand-logo">React Movies</span>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
